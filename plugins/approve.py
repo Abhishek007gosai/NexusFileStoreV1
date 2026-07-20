40ea6b60d5b263088122fdd52fbb8a8b2c69848b
Abhishek007gosai <99321716+Abhishek007gosai@users.noreply.github.com>
2026-07-20 16:11:38 +0530

    Update approve.py

--- plugins/approve.py
+++ plugins/approve.py
@@ -1,4 +1,5 @@
-'''# +++ Modified By Yato [telegram username: @i_killed_my_clan & @ProYato] +++ # aNDI BANDI SANDI JISNE BHI CREDIT HATAYA USKI BANDI RAndi 
+#+++Modified By Yato [telegram username: @i_killed_my_clan & @ProYato] +++ # aNDI BANDI SANDI JISNE BHI CREDIT HATAYA USKI BANDI RAndi 
+
 import os
 import asyncio
 from config import *
